0d140c9431956f3dac9b48481356514645ab2497
watermelonsuperman <15538287230@163.com>
2019-08-22 01:30:16 +0800

    123

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,6 +1,9 @@
-const createMultiplyTable = require('../main');
-it ('should return multiply table from 2 to 4 when call createMultiplyTable given 2 and 4', () => {
-    expect(createMultiplyTable(2,4))
-    .toBe("2*2=4\r\n2*3=6 3*3=9\r\n2*4=8 3*4=12 4*4=16");
-    });
 
+
+    const printReceipt = require('../main');
+    it ('should return receipt', () => {
+        expect(printReceipt(['0001', '0003', '0005', '0003']))
+        .toBe("Receipts\r\n----------------------------------\r\nCoca Cola                    3          1\r\nPepsi-Cola                    5          2\r\nDr Pepper                    7          1\r\n----------------------------------\r\nprice:20");
+        });
+    
+    
--- main.js
+++ main.js
@@ -1,33 +1,86 @@
-function createMultiplyTable(start, end) {
-    var result = "";
-    if (checkInput(start, end) == "true") {
-        result = createTable(start, end);
-    }
-    return result;
+
+function printReceipt(barcodes) {
+    const order = queryOrder(barcodes);
+    return print(order);   
 }
 
-function checkInput(start, end) {
-    var result = "false";
-    if (start >= -2147483648 && start <= 2147483647 && end >= -2147483648 && end <= 2147483647) {
-        result = "true";
-    }
-    return result;
-}
-function createTable(start, end) {
-    var result = "";
-    for (var t = start; t <= end; t++) {
-        for (var i = start; i <= t; i++) {
-            if(i == t && i != 4 && t != 4){
-                result += i + "*" + t + "=" + (t * i) + "\r\n";
-            }else if(i < t){
-                result += i + "*" + t + "=" + (t * i)+" ";
-            }else if(i == t && i == 4 && t == 4){
-                result += i + "*" + t + "=" + (t * i);
+function queryOrder(barcodes){
+    var order = [];
+    var list = [
+        {"id": "0001", "name" : "Coca Cola", "price": 3},
+        {"id": "0002", "name" : "Diet Coke", "price": 4},
+        {"id": "0003", "name" : "Pepsi-Cola", "price": 5},
+        {"id": "0004", "name" : "Mountain Dew", "price": 6},
+        {"id": "0005", "name" : "Dr Pepper", "price": 7},
+        {"id": "0006", "name" : "Sprite", "price": 8},
+        {"id": "0007", "name" : "Diet Pepsi", "price": 9},
+        {"id": "0008", "name" : "Diet Mountain Dew", "price": 10},
+        {"id": "0009", "name" : "Diet Dr Pepper", "price": 11},
+        {"id": "0010", "name" : "Fanta", "price": 12}
+    ]
+    for(let i =  0;i < barcodes.length;i++){
+        for(let j = 0;j < list.length;j++){
+            if(barcodes[i] == list[j].id){
+                order.push(list[j]);
             }
         }
     }
+    return order;
+}
+
+
+//把id和个数存入map中
+function idAndNum(order){
+    const priceAndNumMap = new Map();
+    
+    for(let i = 0;i < order.length;i++){
+        if(priceAndNumMap.get(order[i].id) == null){
+            priceAndNumMap.set(order[i].id,1);
+        }else{
+            priceAndNumMap.set(order[i].id,priceAndNumMap.get(order[i].id)+1)
+        }
+    }
+    return priceAndNumMap;
+}
+
+//总价
+function sumPrice(result){
+    var sum = 0;
+    for(let k = 0;k< result.length;k++){
+        sum += result[k].price*result[k].num;
+    }
+    return sum;
+}
+//组合信息
+function message(priceAndNumMap,order){
+    const result = [];
+    for(let i = order.length-1;i > 0;i++){
+        if(priceAndNumMap.get(order[i].id) > 1){
+            order.splice(i,1);
+            break;
+        }
+    }
+    
+
+    for(let t = 0;t< order.length;t++){
+        result.push({name:order[t].name,price:order[t].price,num:priceAndNumMap.get(order[t].id)});
+    }
     return result;
 
 }
+//输出
+function print(order){
+    const priceAndNumMap = idAndNum(order);
+    const result = message(priceAndNumMap,order);
+    var sum = sumPrice(result);
+   var outPut = "Receipts\r\n----------------------------------\r\n";
+   for(let i = 0;i< result.length;i++){
+       outPut += result[i].name+"                    "+result[i].price+"          "+result[i].num+"\r\n";
 
-module.exports = createMultiplyTable;
+   }
+   outPut += "----------------------------------\r\nprice:"+sum;
+   return outPut;
+}
+
+module.exports = printReceipt;
+
